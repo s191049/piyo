0a236ce29e9c20101e6ed068b0edba834e40aa04
s191049 <minakuchi.shota@g.wakayama-u.jp>
2022-12-10 02:35:41 +0900

    edit config/application.rb & set config/locales/activerecord

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Piyo
+module Fuga
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -18,5 +18,14 @@ module Piyo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    
+    #デフォルト日本語化
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    #ロケールパス
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    
   end
 end
