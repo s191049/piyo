81809f7be752efafa8786845f5674fde6b90191b
s191049 <minakuchi.shota@g.wakayama-u.jp>
2023-01-04 13:30:36 +0900

    searchクリアフォーム作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import jquery from "jquery"
+window.$ = window.jQuery = jquery
 import * as bootstrap from "bootstrap"
