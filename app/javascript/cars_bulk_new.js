0b0c9ba5843992a2848d32806df3082279e2690a
shota <minakuchi.shota@g.wakayama-u.jp>
2023-04-19 21:50:54 +0900

    bulk_newのエンター処理変更

--- app/javascript/cars_bulk_new.js
+++ app/javascript/cars_bulk_new.js
@@ -1,25 +1,5 @@
-/*
-$('select').focusout(function(){
-  var text = $("option:selected", this).text();
-  var car_class = $(this).parent().attr("class");
-  var target_car = car_class.match(/car-\d+/);
-  var target_car_class = "." + target_car;
-  $(target_car_class).each(function(i,elem){
-  	var child_elem = $(elem).find('input');
-  	child_elem.removeClass();
-  	if(text == '軽'){
-  		child_elem.addClass("form-control border-success text-success");
-  	}else{
-  		child_elem.addClass("form-control border-danger text-danger");
-  	}
-  });
-});
-*/
 $('td').focusout(function(){
-//$('select').focusout(function(){
-//$('select').change(function(){
-  //var text = $('option:selected').text();
-  var car_class = "." + $(this).attr("class").match(/car-\d+/)
+  var car_class = "." + $(this).attr("class").match(/car-\d+/);
   var select_elem = $(car_class).find('select');
   var text = $("option:selected", select_elem).text();
   $(car_class).each(function(i,elem){
@@ -30,6 +10,43 @@ $('td').focusout(function(){
   	}else{
   		child_elem.addClass("form-control border-danger text-danger");
   	}
-  	//$(child_elem).css('background-color','red');
   });
+});
+
+
+/*
+$("[class^='car-']").on('keypress', function(event){
+	console.log("aaa");
+	//$(this).keydown(function(event){
+		console.log("hoge");
+		if(event.which === 13){
+			alert("hoge");
+		}
+	});
+});
+*/
+
+//$("[class^='car-'] input").on('keypress', function(event) {
+$("input").on('keypress', function(event) {
+  if (event.which === 13) {
+    if(event.shiftKey){
+    	if(! $('form')[0].reportValidity()){
+    		return false;
+    	}
+    	$('form').submit();
+    	return false;
+    }else{
+  	    event.preventDefault();
+	    var car_class = "." + $(this).parent().attr("class").match(/car-\d+/);
+	    var car_num = car_class.match(/\d+/);
+    	var next_car_num = Number(car_num) + 1;
+	    var next_car_class = ".car-" + next_car_num;
+	    var next_car = $(next_car_class);
+	    if(next_car.length){
+	    	$(next_car_class).find('select').focus();
+	    }else{
+	    }
+	    return false;
+    }    
+  }
 });
